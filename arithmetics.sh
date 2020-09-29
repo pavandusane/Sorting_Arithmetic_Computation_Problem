0a989d5de7da14c37bf1e5cd15facb1df39f8479
pavandusane <dusanepv7@gmail.com>
2020-09-29 22:26:41 +0530

    Added : read value dict to array

--- arithmetics.sh
+++ arithmetics.sh
@@ -8,6 +8,7 @@ echo "Enter 3 Numbers"
 read a
 read b
 read c
+x=1
 
 result1=$(( $a + $b * $c ))
 echo $result1
@@ -25,3 +26,13 @@ dict1[result1]="$result1"
 dict1[result2]="$result2"
 dict1[result3]="$result3"
 dict1[result4]="$result4"
+
+#for (( i=1;i<=${#dict1[@]};i++))
+
+for i in "${dict1[@]}"
+do
+   arr[$x]=$i
+   let x++
+done
+echo ${!arr[@]}
+echo ${arr[@]}
